990f4b66347c0a54a4834869c0f49b7a24d4c6a7
jpj1 <jpj1@aluno.ifal.edu.br>
2023-10-16 20:06:28 -0300

    novo texto!

--- Teste.java
+++ Teste.java
@@ -4,6 +4,6 @@ public class Teste {
     }
     
     public static void main(String[] args) {
-        System.out.println("Hello world um caraio");
+        System.out.println("Hello world, cambada!");
     }
 }
