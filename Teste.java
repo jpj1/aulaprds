c2cc993647f6f957076a8ad1fe1544da8fdca447
elvys <elvys.soares@ifal.edu.br>
2023-10-16 19:52:06 -0300

    incluida nova feature

--- Teste.java
+++ Teste.java
@@ -1,4 +1,8 @@
 public class Teste {
+    public void faznada(){
+        // eu disse que não fazia nada
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello world um caraio");
     }
